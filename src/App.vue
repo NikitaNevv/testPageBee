131980c8c8e93827e8c77e174d584e4ff33726d4
Nick <tortickk8475@gmail.com>
2023-06-10 20:44:33 +0300

    chore: add car photos and create component slider

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-import SectionHeader from './components/sections/sectionHeader/index.vue'
+import SectionHeader from './components/sections/header/index.vue'
+import SectionsPhotoSlider from './components/sections/photoSlider/index.vue'
 </script>
 
 <template>
     <div class="page-main">
         <SectionHeader class="page-main__header"/>
+
+        <SectionsPhotoSlider/>
     </div>
 </template>
 
---
+++ src/components/sections/photoSlider/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {name: 'SectionsPhotoSlider'}
+</script>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
